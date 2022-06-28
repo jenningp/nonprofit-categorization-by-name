9e372c7b0d055e366df9f9082c6feaba01604f2f
jenningp <jenningp@telusplanet.net>
2022-06-28 17:06:28 -0600

    General numbers on data intersections
    How do the Alberta and CRS list intersect as far as common organizations

--- word_comparison.R
+++ word_comparison.R
@@ -4,6 +4,11 @@ library(tidytext)
 library(ggplot2)
 library(readr)
 library(readxl)
+library(tm)
+library(igraph)
+library(ggraph)
+library(widyr)
+
 
 may_non_profit_name_list_for_open_data_portal <- read_excel("may-non_profit_name_list_for_open_data_portal.xlsx")
 View(may_non_profit_name_list_for_open_data_portal)
@@ -27,6 +32,7 @@ cradata <- data.frame(distinct(X2338_Calgary_Charities_CRA2020)) %>%
   unnest_tokens(word, Name, drop = F) %>%
   anti_join(stop_words) %>%
   mutate(source = "CRA charity")
+
 
 frequency_table <- bind_rows(abdata,cradata) %>%
   count(source, word) %>%
@@ -52,7 +58,8 @@ frequency_table_common_1 <- distinct(bind_rows(abdata,cradata)) %>%
   spread(source, n) %>%
   filter(`CRA charity` == 1 & `Alberta Open Data nonprofit Listing` == 1)
 
- 
+
+
 
 frequency_long <- bind_rows(abdata,cradata) %>%
   count(source, word) %>%
@@ -74,15 +81,64 @@ ggplot(frequency_table, aes(x = `CRA charity`, y = `Alberta Open Data nonprofit 
   scale_color_gradient(limits = c(0, 0.001),
                        low = "darkslategray4", high = "gray75") 
 
+v <- (frequency_table$word)
+lapply(v, agrep,v, value=TRUE)
+df <- bind_rows(abdata,cradata) %>%
+  mutate(stemmed = stemDocument(word))
 
-ggplot(frequency_table_common, aes(x = `CRA charity`, y = `Alberta Open Data nonprofit Listing`)) +
+
+
+frequency_table_stemmed <- bind_rows(abdata,cradata) %>%
+  mutate(stemmed = stemDocument(word)) %>%
+  count(source, stemmed) %>%
+  group_by(source) %>%
+  mutate(sum = sum(n)) %>%
+  mutate(proportion = n / sum(n)) %>%
+  select(-n, -sum) %>%
+  spread(source, proportion)
+
+frequency_table_stemmed_zoom <- frequency_table_stemmed %>%
+  filter(`CRA charity` < .001) %>%
+  filter(`Alberta Open Data nonprofit Listing` < .001)
+
+
+ggplot(frequency_table_stemmed, aes(x = `CRA charity`, y = `Alberta Open Data nonprofit Listing`)) +
   geom_abline(color = "gray40", lty = 2) +
   geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
-  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  geom_text(aes(label = stemmed), check_overlap = TRUE, vjust = 1.5) +
   scale_x_log10(labels = scales::percent_format()) +
   scale_y_log10(labels = scales::percent_format()) +
   scale_color_gradient(limits = c(0, 0.001),
                        low = "darkslategray4", high = "gray75") 
+
+ggplot(frequency_table_stemmed_zoom, aes(x = `CRA charity`, y = `Alberta Open Data nonprofit Listing`)) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = stemmed), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = scales::percent_format()) +
+  scale_y_log10(labels = scales::percent_format()) +
+  scale_color_gradient(limits = c(0, 0.001),
+                       low = "darkslategray4", high = "gray75") 
+
+title_word_pairs <- df %>%
+  select(word, stemmed) %>%
+  mutate(merged = paste(word,stemmed)) %>%
+  count(merged) %>%
+  separate(merged, sep = " ", into = c("word", "stemmed"))
+
+write.csv(title_word_pairs, "word_pairs.csv")
+
+
+set.seed(1234)
+title_word_pairs %>%
+  filter(n >= 200) %>%
+  graph_from_data_frame() %>%
+  ggraph(layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "cyan4") +
+  geom_node_point( size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  theme_void()
 
 
   
